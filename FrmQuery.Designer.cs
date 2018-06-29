4f0ae85dd0eb12056fd7d7ef4883e5d9d84e6c83
wsk3q <wsk3q@hotmail.com>
2018-06-30 07:56:36 +0800

    Add project files.

---
+++ FrmQuery.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MyDBQuery
+{
+    partial class FrmQuery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textSql = new System.Windows.Forms.RichTextBox();
+            this.gridData = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.21642F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.splitContainer1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 29.26829F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(576, 372);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(3, 332);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 37);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(368, 3);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(69, 26);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(498, 3);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(69, 26);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnQuery.Location = new System.Drawing.Point(281, 3);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(69, 26);
+            this.btnQuery.TabIndex = 0;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textSql);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.gridData);
+            this.splitContainer1.Size = new System.Drawing.Size(570, 323);
+            this.splitContainer1.SplitterDistance = 123;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // textSql
+            // 
+            this.textSql.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textSql.Location = new System.Drawing.Point(0, 0);
+            this.textSql.Name = "textSql";
+            this.textSql.Size = new System.Drawing.Size(570, 123);
+            this.textSql.TabIndex = 0;
+            this.textSql.Text = "";
+            // 
+            // gridData
+            // 
+            this.gridData.AllowUserToAddRows = false;
+            this.gridData.AllowUserToDeleteRows = false;
+            this.gridData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridData.Location = new System.Drawing.Point(0, 0);
+            this.gridData.Name = "gridData";
+            this.gridData.ReadOnly = true;
+            this.gridData.Size = new System.Drawing.Size(570, 196);
+            this.gridData.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.95751F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.04249F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 87F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 129F));
+            this.tableLayoutPanel2.Controls.Add(this.btnClear, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnExport, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnQuery, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(570, 37);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // FrmQuery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 372);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrmQuery";
+            this.Text = "Oracle Query Test";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox textSql;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView gridData;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ FrmQuery.cs
@@ -0,0 +1,102 @@
+﻿using Common;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MyDBQuery
+{
+    public partial class FrmQuery : Form
+    {
+        private string mOracleConnStr = string.Empty;
+        private DataTable mLastQuery = null;
+        public FrmQuery()
+        {
+            InitializeComponent();
+            mOracleConnStr = ConfigurationManager.ConnectionStrings["OracleConnStr"].ConnectionString;
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            ClearGrid();
+            DoQuery(textSql.Text.Trim());
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearGrid();
+            textSql.Clear();
+            textSql.Focus();
+        }
+        private void ClearGrid()
+        {
+            gridData.DataSource = null;
+        }
+        private void DoQuery(string sSql)
+        {
+            if (string.IsNullOrEmpty(sSql)) { return; }
+            mLastQuery = new DataTable();
+            try
+            {
+                using (var conn = new OracleConnection(mOracleConnStr))
+                {
+                    using (var dr = OraClientHelper.ExecuteReader(conn, CommandType.Text, sSql, null))
+                    {
+                        mLastQuery.Load(dr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "连接Oracle失败");
+                return;
+            }
+            
+            gridData.DataSource = mLastQuery;
+            setRowNumber(gridData);
+        }
+
+        private void setRowNumber(DataGridView dgv)
+        {
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                row.HeaderCell.Value = (row.Index + 1).ToString();
+            }
+            dgv.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if(null== mLastQuery)
+            {
+                ClearGrid();
+                DoQuery(textSql.Text.Trim());
+            }
+            if (null != mLastQuery)
+            {
+                var bys = EPPExcelHelper.BuilderExcel(mLastQuery);
+                if(null!=bys && bys.Length > 0)
+                {
+                    SaveFileDialog saveFileDlg = new SaveFileDialog();
+
+                    saveFileDlg.Filter = "Excel files (*.xlsx)|*.xlsx";
+                    saveFileDlg.FilterIndex = 1;
+                    saveFileDlg.RestoreDirectory = true;
+
+                    if (saveFileDlg.ShowDialog() == DialogResult.OK)
+                    {
+                        Stream myStream;
+                        if ((myStream = saveFileDlg.OpenFile()) != null)
+                        {
+                            myStream.Write(bys, 0, bys.Length);
+                            myStream.Close();
+                            MessageBox.Show("导出完成！");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
